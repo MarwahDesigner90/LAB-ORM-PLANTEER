40f41ac36a36227190f5a51dfc7205490676f6f4
MarwahDesigner90 <marwaty666@gmail.com>
2024-11-06 13:06:34 +0300

    Create templates and project applications as first step

---
+++ PlanteerWebsite/Planteer/main_App/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path
+
+app_name = "main_App"
+
+urlpatterns = [ 
+    path('/' , views.home_view , name="home_view"),
+    path('contact/' , views.contact_view , name="contact_view"),
+    path('contact/messages/',views.messages_view , name="messages_view"),
+
+]
